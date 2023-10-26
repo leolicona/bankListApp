53c08fe6596b00536c6a02973cbbbbe5fbd8489d
Leobardo Licona Soto <leobardo.licona@elektra.com.mx>
2023-10-26 15:32:09 -0600

    Fix: retore endpoint

--- src/modules/BankList/useBankList.js
+++ src/modules/BankList/useBankList.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import useBankSlice from '../../store/bankSlice/useBankSlice'
 import useFetch from '../../hooks/useFetch'
 import { saveToLocalStorage, getFromLocalStorage } from '../../utils/persistData'
-const url = 'dev.obtenmas.com/catom/api/challenge/banks'
+const url = 'https://dev.obtenmas.com/catom/api/challenge/banks'
 
 export default function useBankList () {
   const { banks, setBanksState, setErrorState, setLoadingState } = useBankSlice()
