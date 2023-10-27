e5e1f47127385b8e79bafd6340b3752ce18340bc
Leobardo Licona Soto <leobardo.licona@elektra.com.mx>
2023-10-26 22:33:58 -0600

    Fix: change vite proxy confing

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const proxy = {
+  '/api': {
+    target: 'http://localhost:3000/dev.obtenmas.com/catom/api/challenge/banks',
+    changeOrigin: true,
+    rewrite: path => path.replace(/^\/api/, '')
+  }
+}
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT
+    port: 3000
+    // ...proxy
+    /* proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    } */
   }
-
 })
